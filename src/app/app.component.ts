3f8e43288156109508b000cc615ccb1bd51af6fe
Heitor Miranda <heitormiranda@Heitors-MacBook-Pro.local>
2024-11-26 20:54:58 -0300

    go

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'new-proj';
+  ts_hash_version = '';
+  ts_commit = '';
+  /**
+   *
+   */
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    this.ts_hash_version = (document.defaultView as any)[
+      'ts_hash_version'
+    ] as any;
+    this.ts_commit = (document.defaultView as any)['ts_commit'] as any;
+  }
 }
